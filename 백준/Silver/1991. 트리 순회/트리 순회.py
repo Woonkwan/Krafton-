b7c6a3040497b350d8f5693a61a224edbcc75859
Woonkwan <154650061+Woonkwan@users.noreply.github.com>
2024-01-24 19:37:17 +0900

    [Silver I] Title: 트리 순회, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.py
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -1,55 +1,45 @@
 import sys
 input = sys.stdin.readline
 
-class Node:
-    def __init__(self, data,left = None, right = None):
-        self.data = data
-        self.left = left
-        self.right = right
 
-def pre(node):
-    print(node.data, end = '')
-    if node.left != None:
-        pre(tree[node.left])
-    if node.right != None:
-        pre(tree[node.right])
 
-def ino(node):
-    if node.left != None:
-        ino(tree[node.left])
-    print(node.data, end = '')
-    if node.right != None:
-        ino(tree[node.right])
+#=================================
+def first(node):
+    if node == None:
+        return
+    print(node, end = '')
+    first(tree[node][0])
+    first(tree[node][1])
+#=================================
+def mid(node):
+    if node == None:
+        return
+    mid(tree[node][0])
+    print(node, end = '')
+    mid(tree[node][1])
+#=================================
+def last(node):
+    if node == None:
+        return
+    last(tree[node][0])
+    last(tree[node][1])
+    print(node, end = '')
 
-def pos(node):
-    if node.left != None:
-        pos(tree[node.left])
-    if node.right != None:
-        pos(tree[node.right])
-    print(node.data, end = '')
+#=================================입력
 
-#전위 중위 후회 순으로 함수 정의
-
+N = int(input())
 tree = {}
-
-N  = int(input())
 
 for _ in range(N):
     x, y, z = input().split()
-    if y == '.':
-        y = None
-    if z == '.':
-        z = None
-    tree[x] = Node(x, y, z)
-#N을 받아서 N만큼 정보를 받음.
+    tree[x] = (y if y != '.' else None, z if z != '.' else None)
 
-
-#출력
-pre(tree['A'])
+#=================================출력
+first('A')
 print()
 
-ino(tree['A'])
+mid('A')
 print()
 
-pos(tree['A'])
+last('A')
 print()
