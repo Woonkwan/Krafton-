3d448cdcd47caec4f5aa41498bf40422bd2216c8
Woonkwan <154650061+Woonkwan@users.noreply.github.com>
2024-01-27 15:45:47 +0900

    [Gold V] Title: LCS, Time: 536 ms, Memory: 63560 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,22 +1,17 @@
 import sys
 input = sys.stdin.readline
+s1 = " " + input().strip()
+s2 = " " + input().strip()
+n, m = len(s1)-1, len(s2)-1
+dp = [[0]*(m+1) for _ in range(n+1)]
+#왔던 길을 다시 돌아가 LCS의 문자들을 찾음
+backTrack = [[0]*(m+1) for _ in range(n+1)]
 
-# 두 문자열 입력 받기
-sudle = [input().strip() for _ in range(2)]
-
-# 문자열의 길이에 맞게 DP 테이블 초기화
-N, M = len(sudle[0]), len(sudle[1])
-dp = [[0] * (M+1) for _ in range(N+1)]
-
-# DP를 이용하여 LCS 계산
-for i in range(1, N+1):
-    for j in range(1, M+1):
-        if sudle[0][i-1] == sudle[1][j-1]:
-            # 문자가 일치하면 대각선 위의 값에 1을 더한다
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        if s1[i] == s2[j]:
             dp[i][j] = dp[i-1][j-1] + 1
+            backTrack[i][j] = 1
         else:
-            # 문자가 일치하지 않으면, 왼쪽 또는 위쪽의 최대값을 가져온다
             dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-
-# 결과 출력 (DP 테이블의 마지막 값이 LCS의 길이)
-print(dp[N][M])
+print(dp[n][m])
