9e9a343eb47b4384c75749cf2b052b50f77f9ddb
Woonkwan <154650061+Woonkwan@users.noreply.github.com>
2024-01-31 13:23:49 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -1,17 +1,16 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())
-
-def fibo(n):
-    if n <= 0:
+def fibo(N):
+    if N <= 0:
         return 0
-    elif n == 1:
+    elif N == 1:
         return 1
     else:
-        a, b = 0, 1
-        for _ in range(n-1):
-            a, b = b, a + b
+        a, b = 0 ,1
+        for _ in range(N-1):
+            b , a = a +b , b
         return b
-
+        
+N = int(input())
 print(fibo(N))
