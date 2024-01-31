04c0fd625c2dac362811039959e427c7f15d49a1
Woonkwan <154650061+Woonkwan@users.noreply.github.com>
2024-02-01 01:50:08 +0900

    [Gold V] Title: 평범한 배낭, Time: 3232 ms, Memory: 34972 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,14 +1,14 @@
 import sys
 input = sys.stdin.readline
 
-N, K = map(int, input().split())  #물건 수, K 가방 최대 무게, V 물건 가중치
-items = [list(map(int, input().split())) for _ in range(N)]  #가방안의 아이템 무게[인덱스]에 따라 가중치 넣어줌
+N, K = map(int, input().split())
+W_V = [list(map(int, input().split())) for _ in range(N)]
 
-dp = [0] * ( K + 1 ) #[0, 0, 0,......... 0, 0]
+dp = [0] * (K + 1) 
 
-for item in items:  #물건을 꺼내서 비교할거야!
-    weight, V = item
-    for i in range(K, weight - 1, -1):
-        dp[i] = max(dp[i], dp[i - weight] + V)
+for item in W_V:
+    W, V = item
+    for i in range(K, W - 1, -1): #K부터 (W-1)까지 -1씩 감소
+        dp[i] = max(dp[i], dp[i - W] + V)
 
 print(dp[-1])
