ea42938197f83731022d02a556e66525b6b211d5
Woonkwan <154650061+Woonkwan@users.noreply.github.com>
2024-02-13 02:51:30 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
@@ -3,17 +3,5 @@ input = sys.stdin.readline
 
 A, B, V = map(int, input().split())
 
-left = 1  # 최소한의 날짜
-right = V  # 최대 가능한 날짜
-
-while left <= right:
-    mid = (left + right) // 2  # 이분 탐색을 위한 중간 값 계산
-    height = A * mid - B * (mid - 1)  # 중간 날짜에서의 높이 계산
-    
-    if height >= V:  # 목표 높이에 도달한 경우
-        answer = mid  # 결과 저장
-        right = mid - 1  # 더 작은 범위에서 확인
-    else:
-        left = mid + 1  # 더 큰 범위에서 확인
-
-print(answer)  # 결과 출력
+days = (V - B) / (A - B)  # 도착지까지 날짜 계산
+print(int(days) if days == int(days) else int(days) + 1)  # 정수로 변환하여 출력
